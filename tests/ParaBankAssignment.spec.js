0d4fca8f8edb97ffd677e1b79e13c27561c6045e
Malatipatil <Malatigithub@123>
2026-06-20 12:30:55 +0530

    Creating Account

--- tests/ParaBankAssignment.spec.js
+++ tests/ParaBankAssignment.spec.js
@@ -1,7 +1,10 @@
 const {test, expect} = require('@playwright/test');
 
 test.only('Create Account And Login it then Print the amount', async ({browser})=>
-{
+{    
+      //const USERName   = 'Malati@gmail.com';
+      //const Password = 'Password@2026'; 
+
     //1) Go to Para Bank Home Page
       const context = await browser.newContext();
       const page = await context.newPage();
@@ -9,6 +12,16 @@ test.only('Create Account And Login it then Print the amount', async ({browser})
 
     // Create account 
     await page.getByRole('link', {name: 'Register'}).click();
-    await page.pause();
 
+    await page.locator('xpath=//input[@id="customer.firstName"]').fill('Malati');
+
+    await page.locator('xpath=//input[@id="customer.lastName"]').fill('Chougale');
+
+   /* await page.locator('xpath=//input{@id="customer.address.street"]').fill('Deppnagar');
+    await page.locator('xpath=//input{@id="customer.address.city"]').fill('Bhusawal');
+    await page.locator('xpath=//input{@id="customer.address.zipCode"]').fill('425307');
+    await page.locator('xpath=//input{@id="customer.phoneNumber"]').fill('9158523071');
+    await page.locator('xpath=//input{@id="customer.ssn"]').fill('200626002');*/
+
+    await page.pause();
 });
