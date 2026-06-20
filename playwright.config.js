7a4b836123a38ac273130a0a150bb9682fd44673
Malatipatil <Malatigithub@123>
2026-06-20 14:23:45 +0530

    Account Created and Logged Out

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ import { devices } from '@playwright/test';
  */
 const config = ({ 
   testDir: './tests',
-  timeout: 30 *1000,
+  timeout: 70 *1000,
    expect : {
     timeout: 5000
    }, 
--- tests/ParaBankAssignment.spec.js
+++ tests/ParaBankAssignment.spec.js
@@ -2,8 +2,8 @@ const {test, expect} = require('@playwright/test');
 
 test.only('Create Account And Login it then Print the amount', async ({browser})=>
 {    
-      //const USERName   = 'Malati@gmail.com';
-      //const Password = 'Password@2026'; 
+      const UserName = 'atulCh@gmail.com'; 
+      const Password = 'Atulch@2026';
 
     //1) Go to Para Bank Home Page
       const context = await browser.newContext();
@@ -11,17 +11,25 @@ test.only('Create Account And Login it then Print the amount', async ({browser})
       await page.goto(" https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC");
 
     // Create account 
-    await page.getByRole('link', {name: 'Register'}).click();
+    await page.getByRole('link',{name:'Register' , exact: true}).click();
 
-    await page.locator('xpath=//input[@id="customer.firstName"]').fill('Malati');
-
+    await page.locator('xpath=//input[@id="customer.firstName"]').fill('Atul');
     await page.locator('xpath=//input[@id="customer.lastName"]').fill('Chougale');
+    await page.locator('xpath=//input[@id="customer.address.street"]').fill('Deppnagar3');
+    await page.locator('xpath=//input[@id="customer.address.city"]').fill('Kolhapur');
+    await page.locator('xpath=//input[@id="customer.address.state"]').fill('Maharashtra');
+    await page.locator('xpath=//input[@id="customer.address.zipCode"]').fill('425305');
+    await page.locator('xpath=//input[@id="customer.phoneNumber"]').fill('9158523052');
+    await page.locator('xpath=//input[@id="customer.ssn"]').fill('200626008');
 
-   /* await page.locator('xpath=//input{@id="customer.address.street"]').fill('Deppnagar');
-    await page.locator('xpath=//input{@id="customer.address.city"]').fill('Bhusawal');
-    await page.locator('xpath=//input{@id="customer.address.zipCode"]').fill('425307');
-    await page.locator('xpath=//input{@id="customer.phoneNumber"]').fill('9158523071');
-    await page.locator('xpath=//input{@id="customer.ssn"]').fill('200626002');*/
+   
+    await page.fill('xpath=//input[@id="customer.username"]',UserName);
+    await page.fill('xpath=//input[@id="customer.password"]',Password);
+    await page.fill('xpath=//input[@id="repeatedPassword"]',Password);
+    await page.click('input[value="Register"]');
+
+     //Log Out 
+    await page.getByRole('link',{name:'Log Out', exact: true}).click();
 
     await page.pause();
 });
